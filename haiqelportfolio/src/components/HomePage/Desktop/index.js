a12ac3e922e97c35977c7cf162b183b8dca20d9d
Acerizm <s10172619@connect.np.edu.sg>
2018-11-11 21:16:29 +0800

    1

---
+++ haiqelportfolio/src/components/HomePage/Desktop/index.js
@@ -0,0 +1,15 @@
+import React,{Component} from "react";
+
+//create the body
+export class Body extends Component {
+  // other attributes
+
+  //render
+  render(){
+    return (
+      <div>
+        hello world from the body!
+      </div>
+    )
+  }
+}
---
+++ haiqelportfolio/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Body} from './components/desktop/index.js';
+
+
+ReactDOM.render(<Body/>,document.getElementById('root'));
