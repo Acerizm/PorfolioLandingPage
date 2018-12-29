1da5be9ba685e41fbda043b0c8fd6c1506ea8c5f
Acerizm <34855234+Acerizm@users.noreply.github.com>
2018-12-29 17:18:42 +0800

    My Projects Collection Mobile Completed

--- haiqelportfolio/src/Desktop/components/MyProjectCollection/index.jsx
+++ haiqelportfolio/src/Desktop/components/MyProjectCollection/index.jsx
@@ -15,7 +15,9 @@ import GameIcon from "../../.././Pictures/GameIcon.png"
 import PortfolioIcon from "../../.././Pictures/PortfolioIcon.png"
 import CloudIcon from "../../.././Pictures/CloudIcon.png"
 import EcommerceIcon from "../../.././Pictures/EcommerceIcon.png"
-
+//import the responsive wrapper code here
+import DesktopBreakpoint from "../responsiveWrapperCode/desktop_breakpoint.jsx";
+import PhoneBreakpoint from "../responsiveWrapperCode/phone_breakpoint.jsx";
 export class MyProjectCollection extends Component {
 
   //attributes here
@@ -23,57 +25,103 @@ export class MyProjectCollection extends Component {
 		super(props);
 		// logo tween
 		this.Tween = null;
-    this.test2 = []
+    this.MyProjectsCollectionArray = []
 	}
   componentDidMount(){
 		// create logo tween
 		this.Tween = new TimelineMax({pause:false})
     //.to(this.WhoAmI,1,{rotation: 360})
     //.staggerTo(this.WhoAmI,2,{rotation: 360},2);
-    //.staggerFromTo(this.test2, 0.5,
-      //{ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
-      //{ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.1)
+    .staggerFromTo(this.MyProjectsCollectionArray, 3,
+      {ease: Back.easeOut.config(1.7), opacity: 0, bottom: -80},
+      {ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0}, 0.5)
     //.to(this.MyDesc, 0.5,{ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0,x: 100})
     //.to(this.MyDesc, 0.5,{ease: Back.easeOut.config(1.7), opacity: 1, bottom: 0,x: 0});
 	}
   //render here
   render(){
     return(
-      <div id="MyProjectCollection" style={{height:"600px",paddingBottom: "15%",backgroundColor: "white"}}>
-        <h1 style={{fontFamily:"Arial",textAlign:"center",marginTop:"5%"}}>My Projects Collection</h1>
-        <div id="ProjectsContainer" style={{...ProjectsContainerStyle,marginTop:"2%"}}>
-            <div id="1st Row" style={{...RowStyle}}>
-              {/*There are 2 columns here*/}
-              <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#21A1F0"}}>
-                <img src={StoreIcon} style={{...IconStyle}} ></img>
+      <div>
+        <DesktopBreakpoint>
+        <div id="MyProjectCollection" style={{height:"600px",paddingBottom: "15%",backgroundColor: "white"}}>
+          <h1 style={{fontFamily:"Arial",textAlign:"center",marginTop:"5%"}}>My Projects Collection</h1>
+          <div id="ProjectsContainer" style={{...ProjectsContainerStyle,marginTop:"2%"}}>
+              <div id="1st Row" style={{...RowStyle}}>
+                {/*There are 2 columns here*/}
+                <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#21A1F0"}}>
+                  <img src={StoreIcon} style={{...IconStyle}} ></img>
+                </div>
+                <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#FCCCDC"}}>
+                  <img src={CameraIcon} style={{...IconStyle}} ></img>
+                </div>
               </div>
-              <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#FCCCDC"}}>
-                <img src={CameraIcon} style={{...IconStyle}} ></img>
+              <div class="EmptyDiv" style={{...EmptyDivStyle}}>
               </div>
-            </div>
-            <div class="EmptyDiv" style={{...EmptyDivStyle}}>
-            </div>
-            <div id="2nd Row" style={{...RowStyle}}>
-              {/*There are 2 columns here*/}
-              <div class="smallerColumn" style={{...SmallerColumnStyle,marginLeft: "5%",backgroundColor:"#63C1A0"}}>
-                <img src={GameIcon} style={{...IconStyle}} ></img>
+              <div id="2nd Row" style={{...RowStyle}}>
+                {/*There are 2 columns here*/}
+                <div class="smallerColumn" style={{...SmallerColumnStyle,marginLeft: "5%",backgroundColor:"#63C1A0"}}>
+                  <img src={GameIcon} style={{...IconStyle}} ></img>
+                </div>
+                <div class="biggerColumn" style={{...BiggerColumnStyle,marginLeft: "10%",backgroundColor:"#EBB230"}}>
+                  <img src={PortfolioIcon} style={{...IconStyle}} ></img>
+                </div>
               </div>
-              <div class="biggerColumn" style={{...BiggerColumnStyle,marginLeft: "10%",backgroundColor:"#EBB230"}}>
-                <img src={PortfolioIcon} style={{...IconStyle}} ></img>
+              <div class="EmptyDiv" style={{...EmptyDivStyle}}>
               </div>
-            </div>
-            <div class="EmptyDiv" style={{...EmptyDivStyle}}>
-            </div>
-            <div id="3rd Row" style={{...RowStyle}}>
-              {/*There are 2 columns here*/}
-              <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#E03434"}}>
-                  <img src={CloudIcon} style={{...IconStyle}} ></img>
+              <div id="3rd Row" style={{...RowStyle}}>
+                {/*There are 2 columns here*/}
+                <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#E03434"}}>
+                    <img src={CloudIcon} style={{...IconStyle}} ></img>
+                </div>
+                <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#653EAD"}}>
+                  <img src={EcommerceIcon} style={{...IconStyle}} ></img>
+                </div>
               </div>
-              <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#653EAD"}}>
-                <img src={EcommerceIcon} style={{...IconStyle}} ></img>
-              </div>
-            </div>
+          </div>
         </div>
+        </DesktopBreakpoint>
+        <PhoneBreakpoint>
+          <div id="MyProjectCollection" style={{height:"700px",paddingBottom: "30%",backgroundColor: "white"}}>
+            <h1 style={{fontFamily:"Arial",textAlign:"center",marginTop:"5%",position:"relative"}}>
+              <span ref={span => this.MyProjectsCollectionArray[0] = span}>My </span>
+              <span ref={span => this.MyProjectsCollectionArray[1] = span}>Projects </span>
+              <span ref={span => this.MyProjectsCollectionArray[2] = span}>Collection </span>
+            </h1>
+            <div id="ProjectsContainer" style={{...ProjectsContainerStyle,marginTop:"2%"}}>
+                <div id="1st Row" style={{...RowStyle}}>
+                  {/*There are 2 columns here*/}
+                  <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#21A1F0"}}>
+                    <img src={StoreIcon} style={{...IconStyle}} ></img>
+                  </div>
+                  <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#FCCCDC"}}>
+                    <img src={CameraIcon} style={{...IconStyle}} ></img>
+                  </div>
+                </div>
+                <div class="EmptyDiv" style={{...EmptyDivStyle}}>
+                </div>
+                <div id="2nd Row" style={{...RowStyle}}>
+                  {/*There are 2 columns here*/}
+                  <div class="smallerColumn" style={{...SmallerColumnStyle,marginLeft: "5%",backgroundColor:"#63C1A0"}}>
+                    <img src={GameIcon} style={{...IconStyle}} ></img>
+                  </div>
+                  <div class="biggerColumn" style={{...BiggerColumnStyle,marginLeft: "10%",backgroundColor:"#EBB230"}}>
+                    <img src={PortfolioIcon} style={{...IconStyle}} ></img>
+                  </div>
+                </div>
+                <div class="EmptyDiv" style={{...EmptyDivStyle}}>
+                </div>
+                <div id="3rd Row" style={{...RowStyle}}>
+                  {/*There are 2 columns here*/}
+                  <div class="biggerColumn" style={{...BiggerColumnStyle,backgroundColor:"#E03434"}}>
+                      <img src={CloudIcon} style={{...IconStyle}} ></img>
+                  </div>
+                  <div class="smallerColumn" style={{...SmallerColumnStyle,backgroundColor:"#653EAD"}}>
+                    <img src={EcommerceIcon} style={{...IconStyle}} ></img>
+                  </div>
+                </div>
+            </div>
+          </div>
+        </PhoneBreakpoint>
       </div>
     )
   }
